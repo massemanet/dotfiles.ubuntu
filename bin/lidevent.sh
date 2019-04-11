5ec36480b1d677952d053ec4d26cf618413a88b3
mats cronqvist <masse@cronqvi.st>
2019-04-11 09:59:47 +0200

    more X11 tweaks

--- bin/lidevent.sh
+++ bin/lidevent.sh
@@ -3,10 +3,15 @@
 echo "user - $(whoami) - 1:$1 2:$2 3:$3" >> /tmp/liduevent.log
 DISPLAY=:0 xrandr | grep " conn" >> /tmp/liduevent.log
 
-if [ "$3" == "close" ]
-then DISPLAY=:0 xrandr --output eDP-1 --off ; echo $? >> /tmp/liduevent.log
-else DISPLAY=:0 xrandr --output eDP-1 --auto ;\
-     DISPLAY=:0 ~/bin/3screens.sh ;\
-     echo $? >> /tmp/liduevent.log
+if [ "$3" == "close" ]; then
+    echo -n "turning off eDP-1... " >> /tmp/liduevent.log
+    DISPLAY=:0 xrandr --output eDP-1 --off
+    echo $? >> /tmp/liduevent.log
+else
+    echo -n "turning on eDP-1... " >> /tmp/liduevent.log
+    DISPLAY=:0 xrandr --output eDP-1 --auto
+    echo $? >> /tmp/liduevent.log
+    echo -n "running 3screens.sh... " >> /tmp/liduevent.log
+    DISPLAY=:0 ~/bin/3screens.sh
+    echo $? >> /tmp/liduevent.log >> /tmp/liduevent.log
 fi
-
