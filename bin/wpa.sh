77cc5b5d134c2225e2ffedbbc161ee427f0a6a34
mats cronqvist <masse@cronqvi.st>
2020-12-15 10:45:46 +0100

    wpa

--- bin/wpa.sh
+++ bin/wpa.sh
@@ -4,7 +4,7 @@ INTERFACE="$(iw dev | grep Interface  | cut -f2 -d" ")"
 CFG="/etc/wpa_supplicant/wpa_supplicant-$INTERFACE.conf"
 
 if [ ! -f "$CFG" ]; then
-    cat > "$CFG" <<HERE
+    cat | sudo tee "$CFG" <<HERE
 ctrl_interface=/run/wpa_supplicant
 update_config=1
 HERE
@@ -15,8 +15,12 @@ read -r -p"key: " -s KEY
 CONF=$(wpa_passphrase "$SSID" "$KEY" | grep -v "\#")
 
 echo "$CONF"
-read -r -p"looks good? (y/n):" KEEP
-[ "$KEEP" = "y" ] || exit 0
-echo "$CONF" >> /etc/wpa_supplicant/wpa_supplicant.conf
-
-wpa_supplicant -B -i "$INTERFACE" -c "$CFG"
+if ! grep -q "$CONF" "$CFG"
+then read -r -p"looks good? (y/n):" KEEP
+     [ "$KEEP" = "y" ] || exit 0
+     echo "$CONF" | sudo tee "$CFG"
+     sudo wpa_supplicant -B -i "$INTERFACE" -c "$CFG"
+     sudo dhclient "$INTERFACE"
+else
+    sudo killall -HUP wpa_supplicant
+fi
